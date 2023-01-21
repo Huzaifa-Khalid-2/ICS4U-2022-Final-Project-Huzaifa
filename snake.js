3a0f094efa1d064cf0e724ae720252b2585a25a3
Huzaifa-Khalid <99287277+Huzaifa-Khalid-2@users.noreply.github.com>
2023-01-21 17:05:38 +0000

    update code

--- snake.js
+++ snake.js
@@ -1,3 +1,12 @@
+// #region general utils
+const getRange = length => [...Array(length).keys()]
+const getWithoutLastElement = array => array.slice(0, array.length - 1)
+const areEqual = (one, another) => Math.abs(one - another) < 0.00000000001
+const getRandomFrom = array =>
+  array[Math.floor(Math.random() * array.length)]
+const getLastElement = array => array[array.length - 1]
+// #endregion
+
 // geometry
 class Vector {
   constructor(x, y) {
@@ -280,9 +289,71 @@ const getProjectors = (containerSize, game) =>{
   }
 }
 
-const render = (state) => {
-  console.log('render')
+const getContext = (width, height) => {
+  const [existing] = document.getElementsByTagName('canvas')
+  const canvas = existing || document.createElement('canvas')
+  if (!existing) {
+    getContainer().appendChild(canvas)
+  }
+  const context = canvas.getContext('2d')
+  context.clearRect(0, 0, canvas.width, canvas.height)
+  canvas.setAttribute('width', width)
+  canvas.setAttribute('height', height)
+  return context
 }
+
+const renderCells = (context, cellSide, width, height) => {
+  context.globalAlpha = 0.2
+  getRange(width).forEach(column => getRange(height).forEach(row => {
+    if ((column + row) % 2 === 1) {
+      context.fillRect(column * cellSide, row * cellSide, cellSide, cellSide)
+    }
+  }))
+  context.globalAlpha = 1
+}
+
+const renderFood = (context, cellSide, { x, y }) => {
+  context.beginPath()
+  context.arc(x, y, cellSide / 2.5, 0, 2 * Math.PI)
+  context.fillStyle = '#e74c3c'
+  context.fill()
+}
+
+const renderSnake = (context, cellSide, snake) => {
+  context.lineWidth = cellSide
+  context.strokeStyle = '#3498db'
+  context.beginPath()
+  snake.forEach(({ x, y }) => context.lineTo(x, y))
+  context.stroke()
+}
+
+const renderScores = (score, bestScore) => {
+  document.getElementById('current-score').innerText = score
+  document.getElementById('best-score').innerText = bestScore
+}
+
+const render = ({
+  game: {
+    width,
+    height,
+    food,
+    snake,
+    score
+  },
+  bestScore,
+  projectDistance,
+  projectPosition
+}) => {
+  const [viewWidth, viewHeight] = [width, height].map(projectDistance)
+  const context = getContext(viewWidth, viewHeight)
+  const cellSide = viewWidth / width
+  renderCells(context, cellSide, width, height)
+  renderFood(context, cellSide, projectPosition(food))
+  renderSnake(context, cellSide, snake.map(projectPosition))
+  renderScores(score, bestScore)
+}
+// #endregion 
+
 // #region main
 
 const getInitialState = () => {
@@ -296,9 +367,35 @@ const getInitialState = () => {
   }
 }
 
-const getNewStatePropsOnTick = (props) => {
-  console.log('get new state')
+const getNewStatePropsOnTick = (oldState) => {
+  if (oldState.stopTime) return oldState
+
+  const lastUpdate = Date.now()
+  if (oldState.lastUpdate) {
+    const game = getNewGameState(
+      oldState.game,
+      oldState.movement,
+      lastUpdate - oldState.lastUpdate
+    )
+    const newProps = {
+      game,
+      lastUpdate
+    }
+    if (game.score > oldState.bestScore) {
+      localStorage.setItem('bestScore', game.score)
+      return {
+        ...newProps,
+        bestScore: game.score
+      }
+    }
+    return newProps
+  }
+
+  return {
+    lastUpdate
+  }
 }
+
 const startGame = () => {
   let state = getInitialState()
   const updateState = props => {
@@ -306,21 +403,34 @@ const startGame = () => {
 }
 
 window.addEventListener('resize', () => {
-  console.log('resize')
-})
-window.addEventListener('keydown', ({ which }) => {
-  console.log('keydown: ', which)
-})
-window.addEventListener('keyup', ({ which }) => {
-  console.log('keyup', which)
-})
+    clearContainer()
+    const containerSize = getContainerSize()
+    updateState({ ...containerSize, ...getProjectors(containerSize, state.game) })
+    tick()
+  })
+  window.addEventListener('keydown', ({ which }) => {
+    const entries = Object.entries(MOVEMENT_KEYS)
+    const [movement] = entries.find(([, keys]) => keys.includes(which)) || [undefined]
+    updateState({ movement })
+  })
+  window.addEventListener('keyup', ({ which }) => {
+    updateState({ movement: undefined })
+    if (which === STOP_KEY) {
+      const now = Date.now()
+      if (state.stopTime) {
+        updateState({ stopTime: undefined, lastUpdate: state.time + now - state.lastUpdate })
+      } else {
+        updateState({ stopTime: now })
+      }
+    }
+  })
 
-const tick = () => {
-  const newProps = getNewStatePropsOnTick(state)
-  updateState(newProps)
-  render(state)
- }
- setInterval(tick, UPDATE_EVERY)
+  const tick = () => {
+    const newProps = getNewStatePropsOnTick(state)
+    updateState(newProps)
+    render(state)
+  }
+  setInterval(tick, UPDATE_EVERY)
 }
 // #endregion
 
